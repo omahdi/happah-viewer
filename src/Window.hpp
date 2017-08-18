6aff8ea756bd91c958e2e2b45bb28dad867ea905
Pawel Herman <pawel.herman@kit.edu>
2017-08-18 14:19:04 +0200

    refactor window

--- src/Window.hpp
+++ src/Window.hpp
@@ -12,23 +12,58 @@
 #include <happah/graphics/glad.h>
 #include <happah/graphics/Viewport.hpp>
 #include <GLFW/glfw3.h>//NOTE: Glad must be included before GLFW.
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 
 namespace happah {
 
+//DECLARATIONS
+
+class Window;
+
+inline void onCursorPosEvent(GLFWwindow* handle, double x, double y);
+
+inline void onFramebufferSizeEvent(GLFWwindow* handle, int width, int height);
+     
+inline void onKeyEvent(GLFWwindow* handle, int key, int scancode, int action, int mods);
+
+inline void onMouseButtonEvent(GLFWwindow* handle, int button, int action, int mods);
+     
+inline void onScrollEvent(GLFWwindow* handle, double xoffset, double yoffset);
+
+inline void onWindowSizeEvent(GLFWwindow* handle, int width, int height);
+
+//DEFINITIONS
+
 class Window {
 public:
-     Window(hpuint width, hpuint height, const std::string& title);
+     Window(hpuint width, hpuint height, const std::string& title)
+          : m_handle(glfwCreateWindow(width, height, title.c_str(), 0, 0)), m_viewport(width, height) {
+          if(m_handle == 0) throw std::runtime_error("Failed to create window.");
+          cache()[m_handle] = this;
+          glfwSetCursorPosCallback(m_handle, happah::onCursorPosEvent);
+          glfwSetFramebufferSizeCallback(m_handle, happah::onFramebufferSizeEvent);
+          glfwSetKeyCallback(m_handle, happah::onKeyEvent);
+          glfwSetMouseButtonCallback(m_handle, happah::onMouseButtonEvent);
+          glfwSetScrollCallback(m_handle, happah::onScrollEvent);
+          glfwSetWindowSizeCallback(m_handle, happah::onWindowSizeEvent);
+     }
 
-     ~Window();
+     ~Window() {
+          cache().erase(m_handle);
+          glfwDestroyWindow(m_handle);
+     }
 
-     GLFWwindow* getContext() const;
+     GLFWwindow* getContext() const { return m_handle; }
 
-     Viewport& getViewport();
+     Viewport& getViewport() { return m_viewport; }
 
 private:
-     static std::unordered_map<GLFWwindow*, Window*> s_windows;
+     static std::unordered_map<GLFWwindow*, Window*>& cache() {
+          static std::unordered_map<GLFWwindow*, Window*> s_windows;
+          return s_windows;
+     }
 
      GLFWwindow* m_handle;
      Viewport m_viewport;
@@ -37,17 +72,41 @@ private:
      
      bool ctrlPressed = false;
 
-     void onCursorPosEvent(double x, double y);
+     void onCursorPosEvent(double x, double y) {
+          y = m_viewport.getHeight() - y;
+          if(glfwGetMouseButton(m_handle, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+               m_viewport.rotate(m_x, m_y, x, y);
+               m_x = x;
+               m_y = y;
+          }
+     }
 
-     void onFramebufferSizeEvent(hpuint width, hpuint height);
+     void onFramebufferSizeEvent(hpuint width, hpuint height) { glViewport(0, 0, width, height); }
      
-     void onKeyEvent(int key, int scancode, int action, int mods);
+     void onKeyEvent(int key, int scancode, int action, int mods) {
+          if( (key == GLFW_KEY_LEFT_CONTROL) || (key == GLFW_KEY_RIGHT_CONTROL) ){
+               ctrlPressed = (action != GLFW_RELEASE);
+          }
+     }
 
-     void onMouseButtonEvent(hpint button, hpint action, hpint mods);
+     void onMouseButtonEvent(hpint button, hpint action, hpint mods) {
+          if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+               glfwGetCursorPos(m_handle, &m_x, &m_y);
+               m_y = m_viewport.getHeight() - m_y;
+          }
+     }
      
-     void onScrollEvent(double xoffset, double yoffset);
+     void onScrollEvent(double xoffset, double yoffset) {
+          if(ctrlPressed) m_viewport.zoom(yoffset * 0.01);
+          else {
+               float delta = yoffset * 0.1;
+               Point3D viewDir = m_viewport.getViewDirection();
+               const Vector3D& step = Vector3D(delta * viewDir.x, delta * viewDir.y, delta * viewDir.z);
+               m_viewport.translate(step);
+          }
+     }
 
-     void onWindowSizeEvent(hpuint width, hpuint height);
+     void onWindowSizeEvent(hpuint width, hpuint height) { m_viewport.setSize(width, height); }
 
      friend void onCursorPosEvent(GLFWwindow* handle, double x, double y);
 
@@ -61,18 +120,19 @@ private:
 
      friend void onWindowSizeEvent(GLFWwindow* handle, int width, int height);
 
-};//class Window
+};//Window
 
-void onCursorPosEvent(GLFWwindow* handle, double x, double y);
+inline void onCursorPosEvent(GLFWwindow* handle, double x, double y) { Window::cache()[handle]->onCursorPosEvent(x, y); }
 
-void onFramebufferSizeEvent(GLFWwindow* handle, int width, int height);
+inline void onFramebufferSizeEvent(GLFWwindow* handle, int width, int height) { Window::cache()[handle]->onFramebufferSizeEvent(width, height); }
      
-void onKeyEvent(GLFWwindow* handle, int key, int scancode, int action, int mods);
+inline void onKeyEvent(GLFWwindow* handle, int key, int scancode, int action, int mods){ Window::cache()[handle]->onKeyEvent(key, scancode, action, mods); }
 
-void onMouseButtonEvent(GLFWwindow* handle, int button, int action, int mods);
+inline void onMouseButtonEvent(GLFWwindow* handle, int button, int action, int mods) { Window::cache()[handle]->onMouseButtonEvent(button, action, mods); }
      
-void onScrollEvent(GLFWwindow* handle, double xoffset, double yoffset);
+inline void onScrollEvent(GLFWwindow* handle, double xoffset, double yoffset){ Window::cache()[handle]->onScrollEvent(xoffset, yoffset); } 
 
-void onWindowSizeEvent(GLFWwindow* handle, int width, int height);
+inline void onWindowSizeEvent(GLFWwindow* handle, int width, int height) { Window::cache()[handle]->onWindowSizeEvent(width, height); }
 
 }//namespace happah
+
